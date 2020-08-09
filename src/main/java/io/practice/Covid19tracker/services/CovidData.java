01f6eaec524c77579ce975bc83746758de84b7ee
Abhay <ch.abhay@yahoo.com>
2020-08-09 20:55:56 +0530

    Initial commit

---
+++ src/main/java/io/practice/Covid19tracker/services/CovidData.java
@@ -0,0 +1,11 @@
+package io.practice.Covid19tracker.services;
+
+import java.net.http.HttpClient;
+
+public class CovidData {
+    private static String url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    public void fetchCovidData() {
+        HttpClient client
+    }
+}
---
+++ src/test/java/io/practice/Covid19tracker/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.practice.Covid19tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
