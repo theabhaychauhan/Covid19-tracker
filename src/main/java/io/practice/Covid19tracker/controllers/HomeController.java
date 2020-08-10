2c8b798d970817321c7c97a8321e46c81f7646ad
Abhay <ch.abhay@yahoo.com>
2020-08-10 21:25:30 +0530

    Added tables for each region

--- src/main/java/io/practice/Covid19tracker/controllers/HomeController.java
+++ src/main/java/io/practice/Covid19tracker/controllers/HomeController.java
@@ -1,11 +1,14 @@
 package io.practice.Covid19tracker.controllers;
 
 
+import io.practice.Covid19tracker.models.locationStats;
 import io.practice.Covid19tracker.services.CovidData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -15,7 +18,10 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("LocationStats",  covidData.getAllStats());
+        List<locationStats> allStats = covidData.getAllStats();
+        int totalCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("LocationStats",  allStats);
+        model.addAttribute("TotalCases", totalCases);
         return "home";
     }
 }
--- src/main/java/io/practice/Covid19tracker/services/CovidData.java
+++ src/main/java/io/practice/Covid19tracker/services/CovidData.java
@@ -43,7 +43,6 @@ public class CovidData {
             locationStat.setState(record.get("Province/State"));
             locationStat.setCountry(record.get("Country/Region"));
             locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
-            System.out.println(locationStat);
             newStats.add(locationStat);
         }
         this.allStats = newStats;
